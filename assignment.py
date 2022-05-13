b9790964e52fd8f9644261a9c6b3a9eb7d95458d
SasakawaDaisuke <sasakawa.daisuke.0714@gmail.com>
2022-05-13 14:55:31 +0900

    勉強1日目

--- assignment.py
+++ assignment.py
@@ -4,11 +4,10 @@ print("Hello World")
 hello = "Hello"
 world = "World"
 
-print(hello + world)
+print(hello + " " + world)
 
 # format
 print("{} {}".format(hello, world))
 
 # fstring python_version >= 3.5
-
 print(f"{hello} {world}")
---
+++ string_basic.py
@@ -0,0 +1,22 @@
+# 文字列(Strings)
+
+print("Hello world")
+print('Hello world')
+
+print(1)
+
+print("I'm fine")
+
+print("""
+Hello
+world
+""")
+
+print("Hello \nworld")
+print("Hello \tworld")
+
+print("\\n")
+
+print('I\'m fine')
+
+print("hello" + "world")
